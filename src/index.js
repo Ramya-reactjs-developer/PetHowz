6beabe52f3fce2913ad9980f34594b9a060af9b2
VigneshFront-EndDB <vignesh.ramakrishnan@doodleblue.in>
2023-02-17 17:03:19 +0530

    platformsetup

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import HomePage from "./Pages/Home/Index";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<HomePage />);
