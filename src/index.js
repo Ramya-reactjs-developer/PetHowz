c4df01e0bd9b8fb2e75290eadd01dc71402c621b
VigneshFront-EndDB <vignesh.ramakrishnan@doodleblue.in>
2023-02-21 16:07:16 +0530

    check

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import CardsSection from "./Components/Cards/Cards";
+import { HomePage } from "./Pages/Home/Index";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<CardsSection />);
+root.render(<HomePage />);
