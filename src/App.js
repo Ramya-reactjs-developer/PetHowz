97ecd923c67a9d9204ccffa7b96f2c05c0f0c7f5
VigneshFront-EndDB <vignesh.ramakrishnan@doodleblue.in>
2023-02-24 12:19:35 +0530

    router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "./Assets/Theme/Index";
-import { HomePage } from "./Pages/Home/Index";
-// import Masters from './Pages/Masters/Index';
+import { RouterNavigation } from "./Router/Index";
+
 function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
-        {/* <Masters/> */}
-        <HomePage />
+        <RouterNavigation />
       </ThemeProvider>
     </>
   );
--- src/Pages/Home/Index.js
+++ src/Pages/Home/Index.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import CardsSection from "../../Components/Cards/Cards";
-import { Box, Grid } from "@mui/material";
+import { Box, Grid, Typography } from "@mui/material";
+import SearchBar from "../../Components/Search/SearchBar";
+import { CustomImage } from "../../Utils/Images/Index";
 
 export const HomePage = () => {
   const CardData = [
@@ -117,10 +119,77 @@ export const HomePage = () => {
       price: "₹ 450",
     },
   ];
+  const [searchData, setSearchData] = useState("");
+  const [iconValue, seticonValue] = useState(false);
+  console.log(searchData, "ss");
+  const OnSetSearch = (e) => {
+    setSearchData(e.target.value);
+  };
+  const onSetIcon = () => {
+    if (searchData !== "") {
+      seticonValue(true);
+    } else {
+      seticonValue(false);
+    }
+  };
+  console.log(iconValue, "v");
+  useEffect(() => {
+    onSetIcon();
+  }, [searchData, iconValue]);
   return (
     // <div>
     //   HomePage
     <Grid item container>
+      <Grid item xl={12} lg={12} md={12}>
+        <Box
+          sx={{
+            width: "100%",
+            height: "50vh",
+            textAlign: "center",
+          }}
+        >
+          <Box
+          // sx={{
+          //   backgroundImage: `url("../../Assets/Images/Screenshot (1) - Copy.png")`,
+          //   backgroundRepeat: "no-repeat",
+          //   backgroundSize: "100px",
+          // }}
+          >
+            <img src={CustomImage.Bg} alt="bg" width={100} />
+            <img src={CustomImage.Screenshot} alt="bg" width={100} />
+            <Typography variant="h3" fontWeight="bolder" color="primary">
+              FIND PET BOARDING SPACES
+            </Typography>
+            <Typography variant="h3" fontWeight="bolder" color="primary">
+              & PET SERVICE PROVIDERS
+            </Typography>
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "space-evenly",
+            }}
+          >
+            <Box>
+              <SearchBar
+                SearchValue={searchData}
+                handleSearch={(e) => {
+                  OnSetSearch(e);
+                  onSetIcon();
+                }}
+                iconValue={iconValue}
+              />
+            </Box>
+            <Box>
+              {/* <CustomButton
+                btnTitle="Search Now"
+                color="primary"
+                fontSize="12px"
+              /> */}
+            </Box>
+          </Box>
+        </Box>
+      </Grid>
       <Grid item>
         <Box>
           <CardsSection Data={CardData} />
